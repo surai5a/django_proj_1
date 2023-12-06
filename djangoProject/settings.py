c045ff7def3e2d8b9847af22af46d8c1a2b475d3
Surai5a <vadym.prymary@gmail.com>
2023-12-06 15:15:23 +0300

    Set up for mac

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +90,8 @@ DATABASES = {
     }
 }
 
+pymysql.version_info = (1, 4, 3, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
